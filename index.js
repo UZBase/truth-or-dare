dec585e993dc94d4fb423f1744db0cb4d64c3024
Umarchek <sharofiddinov.07@mail.ru>
2022-02-05 17:18:51 +0500

    ..

---
+++ index.js
@@ -0,0 +1,71 @@
+require('dotenv').config()
+const { Telegraf, Markup } = require("telegraf");
+const bot = new Telegraf(process.env.TOKEN)
+
+
+function rd() {
+    const rd = Math.floor(Math.random() * 2)
+
+}
+bot.hears("/start", ctx => {
+    const chatId = ctx.chat.id
+    const first_name = ctx.message.from.first_name
+    const userid = ctx.message.from.id
+    const username = ctx.message.from.username
+    const user = `[${ctx.message.from.first_name}](tg://user?id=${ctx.message.from.id})`
+    ctx.deleteMessage();
+
+    // bot.telegram.sendMessage(`${[message.from.first_name]}`, (tg://user?id=message.from.id), { parse_mode: 'MarkdownV2' });
+
+    ctx.telegram.sendPhoto(
+        chatId,
+        { source: './photos/photoStart.png' },
+        {
+            caption: `Добро пожаловать 🥳 ${user} \nВ боте нет глупых вопросов и скучных заданий 😈\nПриятной игры ♥️ `, reply_markup: {
+                inline_keyboard: [
+                    [{ text: "🙈 Правда", callback_data: "Truht" }, { text: "🤯 Действия", callback_data: "Dare" }],
+                    [{ text: "🖇 Случайно", callback_data: "Random" }],
+                    [{ text: "😝 Добавить в группу", url: "https://t.me/Pravda_deystviya_bot?startgroup=new" }],
+                ]
+            }, parse_mode: 'MarkdownV2'
+        }
+    )
+
+    console.log(ctx.chat);
+})
+
+bot.action("Truht", (ctx) => {
+    ctx.deleteMessage();
+    ctx.telegram.sendPhoto(
+        ctx.chat.id,
+        { source: './photos/photoStart.png' },
+        {
+            caption: 'Как вы назвали вашего первого ребёнка ?!\n(Мальчик,девушка)', reply_markup: {
+                inline_keyboard: [
+                    [{ text: "🤯 Действия", callback_data: "Dare" }],
+                    [{ text: "🔜 Следующий ", callback_data: "nextT" }]
+                ]
+            }
+        }
+    )
+
+});
+bot.action("Dare", (ctx) => {
+    ctx.deleteMessage();
+
+
+    ctx.telegram.sendPhoto(
+        ctx.chat.id,
+        { source: './photos/photoStart.png' },
+        {
+            caption: 'Скажи ЛП/ЛД что ты выиграл 100.000сум.Потом скажи это оказывается не ты.', reply_markup: {
+                inline_keyboard: [
+                    [{ text: "🙈 Правда", callback_data: "Truht" }],
+                    [{ text: "🔜 Следующий", callback_data: "nextD" }]
+                ]
+            }
+        }
+    )
+});
+
+bot.launch()
